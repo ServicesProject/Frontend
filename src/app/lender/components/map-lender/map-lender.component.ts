9d6315b171dd5d7c03055de14abb817ab1a16a94
MercedesLaFuente <mercedes.htcv@gmail.com>
2023-04-20 17:51:24 -0400

    markers in the map

--- src/app/lender/components/map-lender/map-lender.component.ts
+++ src/app/lender/components/map-lender/map-lender.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { WorkService } from '../../services/work.service';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-map-lender',
   templateUrl: './map-lender.component.html',
@@ -18,10 +19,12 @@ export class MapLenderComponent implements OnInit {
   area
  
   zoom = 15
-  map:any
+  map: google.maps.Map;
+  radius: number;
+  circle: google.maps.Circle;
   center: google.maps.LatLngLiteral
-  radius = 1000
-  
+  distance
+ 
   @ViewChild('map') mapElement: ElementRef
   constructor(
     private workService: WorkService,
@@ -34,15 +37,60 @@ export class MapLenderComponent implements OnInit {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       };
+      if (this.mapElement?.nativeElement) {
+        this.map = new google.maps.Map(this.mapElement.nativeElement, {
+          zoom: this.zoom,
+          center: this.center,
+        });
+      }
     });
     this.getAllWork()
-    
   }
+
+  convertJStoLatleng(works){
+    let worklist = []
+    let ubication: google.maps.LatLngLiteral
+    works.forEach(element => {
+      ubication = {lat:parseFloat(element.lat), lng:parseFloat(element.lng)}
+      let worktoSave = {... element, ubication: ubication  }
+      worklist.push(worktoSave)
+    });
+    return worklist
+  }
+
+  setRadius() {
+    if (this.circle) {
+      this.circle.setMap(null);
+    }
+    this.radius = this.distance;
+    this.circle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      center: this.center,
+      radius: this.radius,
+      map: this.map,
+      draggable: true,
+    });
+
+    // Agregar controladores de eventos para permitir que el usuario arrastre y cambie el radio del círculo
+    google.maps.event.addListener(this.circle, "center_changed", () => {
+      this.center = this.circle.getCenter().toJSON();
+    });
+
+    google.maps.event.addListener(this.circle, "radius_changed", () => {
+      this.radius = this.circle.getRadius();
+    });
+    //llamar a filteredWorks :) tam poto
+  }
+
 
   getAllWork(){
     this.workService.list().subscribe(
       data => {
-        this.workList = data
+        this.workList = this.convertJStoLatleng(data)
       },
       err => {
         console.log(err)
@@ -55,12 +103,8 @@ export class MapLenderComponent implements OnInit {
   }
 
   filterWorks(){
-    console.log(this.category);
     let worktoSend = {}
-    if(this.area){
-      worktoSend = {...worktoSend, area:this.area}
-    }
-
+    
     if(this.experience){
       worktoSend = {...worktoSend, experience:this.experience}
     }
@@ -77,9 +121,16 @@ export class MapLenderComponent implements OnInit {
       worktoSend = {...worktoSend, category:this.category}
     }
   
-    this.workService.FilterWorksintheMap(worktoSend).subscribe(resp=> this.workList=resp)
-    console.log(this.workList);
-    
+    this.workService.FilterWorksintheMap(worktoSend).subscribe(resp=> {
+      // let filteredworksbyradius = this.filterByMap(resp)
+      
+      // this.workList=filteredworksbyradius
+      this.workList= this.convertJStoLatleng(resp)
+      console.log(this.workList);
+    })
+  }
+  filterByMap(works) {
+
   }
 
 }
